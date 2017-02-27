eb0acbc48e683362829ec83c791772ab0d327482
Sean Whitton <spwhitton@spwhitton.name>
2017-02-27 10:02:24 -0700

    add synopsis and GPL headers

--- Types/State.hs
+++ Types/State.hs
@@ -1,3 +1,26 @@
+{-
+
+sscan --- text UI for scanning with SANE
+
+Copyright (C) 2017  Sean Whitton
+
+This file is part of sscan.
+
+sscan is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+sscan is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with sscan.  If not, see <http://www.gnu.org/licenses/>.
+
+-}
+
 {-# LANGUAGE TemplateHaskell #-}
 
 module Types.State where
