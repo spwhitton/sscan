751303aa5effc5277a89bcbdeedb338f396d8f0b
Sean Whitton <spwhitton@spwhitton.name>
2017-02-28 20:22:10 -0700

    rewrite ifScanSess

--- Types/State.hs
+++ Types/State.hs
@@ -86,7 +86,7 @@ data St =
 makeLenses ''St
 
 ifScanSess :: St -> a -> a -> a
-ifScanSess st a b = if isJust $ st^.stScanSess then a else b
+ifScanSess st a b = maybe b (const a) (st^.stScanSess)
 
 -- | Update a state when there is no scanning session in progress (the
 -- state should not be changed when some pages have already been
